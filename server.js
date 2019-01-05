7436fa6f417221785c50adf64d170a13ecd79598
bintang <bahybintang@gmail.com>
2019-01-05 22:02:02 +0700

    .

--- server.js
+++ server.js
@@ -5,16 +5,25 @@ var line = require('@line/bot-sdk')
 
 var prayerTimes = prayer.getTimes(new Date(), [-7.797068, 110.370529, 113], 7, 0, "24h")
 
+var user
+
 const config = {
     channelAccessToken: 'QPpBgdobbUDWp/UbPt4ejatkYIqKnBkSl4o1ZMTuxhsCI/Y9o8KE/LrRqTwl33m4hh/oaVbN1JJaQWeDtbkCUe6+AdOgQiPh6B9JiY8B742vlwyKGiwdTvw645gqBb3JVHh0YipcMT9AMmiG65Ia8gdB04t89/1O/w1cDnyilFU=',
     channelSecret: '8bce73487a599e8b2852357e49d803e3'
 }
 
+const client = new line.Client(config);
+
 schedule.scheduleJob("1 0 * * *", () => {
     prayerTimes = prayer.getTimes(new Date(), [-7.797068, 110.370529, 113], 7, 0, "24h")
 })
 
-const client = new line.Client(config);
+schedule.scheduleJob("15 * * * * *", () => {
+    client.multicast(user, {
+        type: "text",
+        text: "test"
+    })
+})
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -45,9 +54,13 @@ function handleEvent (event) {
             var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
             return client.replyMessage(event.replyToken, {
                 type : "text",
-                text : `${String.fromCodePoint(0x1000A8)} Waktu sholat untuk ${utc} ${String.fromCodePoint(0x1000A8)} \nSubuh   : ${prayerTimes.fajr}\nDhuhur  : ${prayerTimes.dhuhr}\nAshar   : ${prayerTimes.asr}\nMaghrib : ${prayerTimes.maghrib}\nIsya    : ${prayerTimes.isha}`
+                text : `${String.fromCodePoint(0x1000A8)} Waktu sholat untuk ${utc} ${String.fromCodePoint(0x1000A8)} \n\nSubuh\t: ${prayerTimes.fajr}\nDhuhur\t: ${prayerTimes.dhuhr}\nAshar\t: ${prayerTimes.asr}\nMaghrib\t: ${prayerTimes.maghrib}\nIsya\t: ${prayerTimes.isha}`
             })
         }
+    }
+    else if (event.type === "follow") {
+        var source = event.source.userId ? event.source.userId : event.source.groupId
+        user.push(source)
     }
     else {
         return Promise.resolve(null);
