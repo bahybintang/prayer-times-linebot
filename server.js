0bf7c30ca3088caf8d3a4bed8c65e10a6ba9876c
bintang <bahybintang@gmail.com>
2019-01-05 23:01:12 +0700

    .

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var schedule = require('node-schedule')
 var prayer = require('./PrayerTimes')
 var app = require('express')()
+var _ = require('lodash')
 var line = require('@line/bot-sdk')
 
 var prayerTimes = prayer.getTimes(new Date(), [-7.797068, 110.370529, 113], 7, 0, "24h")
@@ -14,21 +15,9 @@ const config = {
 
 const client = new line.Client(config);
 
-schedule.scheduleJob("1 0 * * *", () => {
-    prayerTimes = prayer.getTimes(new Date(), [-7.797068, 110.370529, 113], 7, 0, "24h")
-})
+scheduling();
 
-schedule.scheduleJob("*/15 * * * * *", () => {
-    console.log("casted")
-    if(user.length !== 0){
-        client.multicast(user, {
-            type: "text",
-            text: "test"
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-})
+schedule.scheduleJob("init", "1 0 * * *", scheduling)
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -40,6 +29,10 @@ app.post('/', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
+})
+
+app.get('/', (req, res) => {
+    res.send({ message : "hehe" })
 })
 
 function handleEvent (event) {
@@ -75,4 +68,71 @@ function handleEvent (event) {
     else {
         return Promise.resolve(null);
     }
+}
+
+function scheduling() {
+    prayerTimes = prayer.getTimes(new Date(), [-7.797068, 110.370529, 113], 7, 0, "24h")
+    
+    const jobNames = _.keys(schedule.scheduledJobs);
+    for(let name of jobNames){
+        console.log(name)
+        if(name !== 'init'){
+            schedule.cancelJob(name)
+        }
+    }
+
+    schedule.scheduleJob("dhuhr", `${prayerTimes.dhuhr.slice(':')[1]} ${prayerTimes.dhuhr.slice(':')[0]} * * *`, () => {
+        if(user.length !== 0){
+            client.multicast(user, {
+                type: "text",
+                text: `[Waktunya Dzuhur, Sholat lur]\n\n📢 Hayya 'alassholaah 📢\n"Suara adzan sudah berkumandang, itu tandanya Allah mengundang. Yuk lur, segera hadir menghadap-Nya."\n\nAmbil air wudhu-niatkan sholat dzuhur-sholatlah dengan khusyuk.\n\n#DzuhurTime\n#LillahiTa'ala`
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    })
+
+    schedule.scheduleJob("asr", `${prayerTimes.asr.slice(':')[1]} ${prayerTimes.asr.slice(':')[0]} * * *`, () => {
+        if(user.length !== 0){
+            client.multicast(user, {
+                type: "text",
+                text: `[Waktunya Ashar]\n\n📢Hayya 'alassholaah📢\nAdzan sudah berkumandang, Mari tegakkan sholat wahai hamba Allah ☺\n\nJadikanlah sholat itu sebagai kebutuhan, bukan penggugur kewajiban. InsyaAllah hidup ini akan terasa lengkap jika kebutuhan kita terpenuhi☺\n\nSo..\nPenuhi panggilan adzan, ambil air wudhu, niatkan sholat, sholatlah dengan khusyuk.\n\nSemoga ridho Allah selalu mengiringi disetiap ibadah kita, Aamiin😊`
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    })
+
+    schedule.scheduleJob("fajr", `${prayerTimes.fajr.slice(':')[1]} ${prayerTimes.fajr.slice(':')[0]} * * *`, () => {
+        if(user.length !== 0){
+            client.multicast(user, {
+                type: "text",
+                text: `[Waktunya Subuh, Sholat itu lebih baik daripada tidur]\n\n📢 Asholatu khairum minannaum 📢\n\n"Suara adzan sudah berkumandang, itu tandanya kita sudah diundang, oleh siapa? Ya, oleh-Nya."\n\nSo..\nPenuhi undangan-Nya, dengan mengambil air wudhu, lalu niatkan sholat subuh, sholatlah dengan khusyuk.\n\n#SemogaRidhoAllahSelaluMengiringiIbadahHamba-Nya. Aamiin😊`
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    })
+
+    schedule.scheduleJob("isha", `${prayerTimes.isha.slice(':')[1]} ${prayerTimes.isha.slice(':')[0]} * * *`, () => {
+        if(user.length !== 0){
+            client.multicast(user, {
+                type: "text",
+                text: `[Waktunya Isya]\n\nHayya 'alassholaah📢\nAdzan sudah berkumandang, Mari tegakkan sholat wahai hamba Allah ☺\n\nJadikanlah sholat itu sebagai kebutuhan, bukan penggugur kewajiban. InsyaAllah hidup ini akan terasa lengkap jika kebutuhan kita terpenuhi☺\n\nPenuhi panggilan adzan, ambil air wudhu, niatkan sholat, sholatlah dengan khusyuk.\n\nSemoga ridho Allah selalu mengiringi disetiap ibadah kita, Aamiin😊`
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    })
+
+    schedule.scheduleJob("maghrib", `${prayerTimes.maghrib.slice(':')[1]} ${prayerTimes.maghrib.slice(':')[0]} * * *`, () => {
+        if(user.length !== 0){
+            client.multicast(user, {
+                type: "text",
+                text: `[Waktunya Maghrib, Sholat sob]\n\n📢Hayya 'alassholaah📢\nAdzan sudah berkumandang, Mari tegakkan sholat wahai hamba Allah ☺\n\nJadikanlah sholat itu sebagai kebutuhan, bukan penggugur kewajiban. InsyaAllah hidup ini akan terasa lengkap jika kebutuhan kita terpenuhi☺\n\nSo..\n\nPenuhi panggilan adzan, ambil air wudhu, niatkan sholat, sholatlah dengan khusyuk.\n\nSemoga ridho Allah selalu mengiringi disetiap ibadah kita, Aamiin😊`
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    })
 }
